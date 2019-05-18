c0034e0d7d91d081426dcaf9c5226241beeb0ba4
tonysugent <46937770+tonysugent@users.noreply.github.com>
2019-05-17 20:10:35 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,13 +1,15 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
+
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    social_id = db.Column(db.String(64), nullable=True, unique=True)
     email = db.Column(db.String(255), unique=True)
     firstname = db.Column(db.String(255), unique=False)
     lastname = db.Column(db.String(255), unique=False)
+
+    def __repr__(self):
+        return f"User('{self.id}','{self.email}','{self.firstname}','{self.lastname}')"
+
