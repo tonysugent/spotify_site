49e368a5c55db3b328048e1f693f01db569b6ac1
tonysugent <46937770+tonysugent@users.noreply.github.com>
2019-05-17 20:11:24 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-import views
+from . import views
 from flask.ext.via.routers import default, Include
 
 routes = [
     default.Functional('/', views.home),
-    default.Functional('/top_played', views.top_played)
+    default.Functional('/top_played+<accesstoken>', views.top_played),
+    default.Functional('/callback/q', views.callback)
 ]
